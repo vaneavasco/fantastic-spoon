e62e2d035bc8c0de7713609d22a3cacb997098e4
vaneavasco <vaneavasco@outlook.com>
2017-12-30 17:24:25 +0200

    heroku deployment

--- server/server.js
+++ server/server.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const {Todo} = require('./models/todo');
 const {ObjectID} = require('mongodb');
+
+const {mongoose} = require('./db/mongoose');
+const {Todo} = require('./models/todo');
+const {User} = require('./models/user');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -9,44 +12,45 @@ const port = process.env.PORT || 3000;
 app.use(bodyParser.json());
 
 app.post('/todos', (req, res) => {
-    let todo = new Todo({text: req.body.text});
-    todo.save().then((doc) => {
-            res.send(doc);
-        },
-        (err) => {
-            res.status(400).send(err);
-        }
-    );
+    let todo = new Todo({
+        text: req.body.text
+    });
 
+    todo.save().then((doc) => {
+        res.send(doc);
+    }, (e) => {
+        res.status(400).send(e);
+    });
 });
 
 app.get('/todos', (req, res) => {
     Todo.find().then((todos) => {
         res.send({todos});
-    }, (err) => {
-        res.status(400).send(err);
+    }, (e) => {
+        res.status(400).send(e);
     });
 });
 
 app.get('/todos/:id', (req, res) => {
     let id = req.params.id;
+
     if (!ObjectID.isValid(id)) {
-        return res.status(400).send();
+        return res.status(404).send();
     }
 
     Todo.findById(id).then((todo) => {
         if (!todo) {
             return res.status(404).send();
         }
+
         res.send({todo});
-    }, (err) => {
-        res.status(400).send({err});
+    }).catch((e) => {
+        res.status(400).send();
     });
 });
 
 app.listen(port, () => {
-    console.log(`started at port ${port}`)
+    console.log(`Started up at port ${port}`);
 });
-
 
 module.exports = {app};
